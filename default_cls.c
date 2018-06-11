5e87a6757eb3712e458d964d2ac66f1e8ab32781
Ciro S. Costa <ciro.costa@liferay.com>
2018-06-11 00:31:35 +0000

    Removes unused offset calc

--- default_cls.c
+++ default_cls.c
@@ -3,7 +3,7 @@
 __section("classifier") int cls_main(struct __sk_buff* skb)
 {
 	int ret    = 0;
-	int nh_off = BPF_LL_OFF + ETH_HLEN;
+	int nh_off = ETH_HLEN;
 
 	if (skb->protocol == __constant_htons(ETH_P_IP)) {
 		ret = l4_show_ports(skb, nh_off);
