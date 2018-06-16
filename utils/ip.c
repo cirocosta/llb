673e9428b94b7f3970d94ce5b65e4550b2c2e623
Ciro S. Costa <ciro.costa@liferay.com>
2018-06-16 13:02:28 +0000

    Adds ipv4 printing utility

---
+++ utils/ip.c
@@ -0,0 +1,34 @@
+#include "../classifier/ip.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char** argv)
+{
+	__u8 ip_addr_le[4] = { 0 };
+	__u8 ip_addr_be[4] = { 0 };
+        long  addr;
+
+	if (argc < 2) {
+		printf("Usage: ip <decimal>\n");
+		return 1;
+	}
+
+	addr = strtol(argv[1], NULL, 10);
+	ip_extract_address_be(addr, ip_addr_be);
+	ip_extract_address_le(addr, ip_addr_le);
+
+	printf("big endian:     %u.%u.%u.%u\n",
+	       ip_addr_be[0],
+	       ip_addr_be[1],
+	       ip_addr_be[2],
+	       ip_addr_be[3]);
+	printf("little endian:  %u.%u.%u.%u\n",
+	       ip_addr_le[0],
+	       ip_addr_le[1],
+	       ip_addr_le[2],
+	       ip_addr_le[3]);
+
+	return 0;
+}
