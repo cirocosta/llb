63207598f9ba105cbed5ee022c5a3f746de7e687
Ciro S. Costa <ciro.costa@liferay.com>
2018-06-18 12:40:16 +0000

    Fixes travis machine setup

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -4,11 +4,23 @@ set -o errexit
 set -o xtrace
 
 main() {
+  setup_llvm_repo
   setup_apt_deps
   setup_iproute2
 }
 
+setup_llvm_repo() {
+  wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+
+  echo "# 6.0
+deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main
+deb-src http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main
+deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu trusty main" |
+    sudo tee --append /etc/apt/sources.list
+}
+
 setup_apt_deps() {
+  sudo apt update -y
   sudo apt install -y \
     bc \
     binutils-dev \
